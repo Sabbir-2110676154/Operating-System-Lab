b1109a0883e3812be2cefa596f2dd78a72ef106a
Sabbir Ahmed <114326056+Sabbir-2110676154@users.noreply.github.com>
2025-07-07 11:47:41 +0600

    Update server_fork.c

---
+++ Operating System All Assignment/Ass-22/server_fork.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+int main() {
+    int s = socket(AF_INET, SOCK_STREAM, 0), c;
+    struct sockaddr_in srv = {AF_INET, htons(8080), INADDR_ANY};
+    bind(s, (void*)&srv, sizeof(srv));
+    listen(s, 5);
+    while (1) {
+        c = accept(s, 0, 0);
+        if (fork() == 0) {
+            write(c, "Hi from child\n", 14);
+            close(c);
+            return 0;
+        }
+    }
+}
