165b1e3e79929e2f9ecbf16a2e881b9cfe8c154f
Sabbir Ahmed <114326056+Sabbir-2110676154@users.noreply.github.com>
2025-07-12 16:40:38 +0600

    Update Ass-4.c

--- Operating System All Assignment/Ass-4.c
+++ Operating System All Assignment/Ass-4.c
@@ -1,24 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>   
+#include <stdlib.h>  
+#include <unistd.h> 
+#include<sys/wait.h>
 
-int main() {
-    pid_t pid;
-    for (int i = 1; i <= 3; i++) {
-        pid = fork();
-        if (pid == 0) {
-            printf("child_%d (PID: %d, PPID: %d)\n", i, getpid(), getppid());
-            exit(0);
+
+int main(int argc,char *argv[]) {
+int n;
+scanf("%d",&n);
+    pid_t pid[n+1];
+ 
+for(int i=0;i<n;i++){
+    pid[i] = fork();
+
+    if (pid[i] == 0) { // Child 1
+     printf("child %d PID %d terminating\n", i,getpid());
+
+        exit(EXIT_SUCCESS);
+    } else { 
+if(i==n){
+            for(int i=1;i<=n;i++){
+            wait(NULL);
+
+}  
+ printf("Parent PID %d terminating\n", getpid());
+                
+            }
         }
     }
-    sleep(2); // Let children print first
-    printf("parent_process (PID: %d)\n", getpid());
-    for (int i = 0; i < 3; i++) wait(NULL);
     return 0;
 }
-/*Sample output:
-child_1 (PID: 1234, PPID: 1231)
-child_2 (PID: 1235, PPID: 1231)
-child_3 (PID: 1236, PPID: 1231)
-parent_process (PID: 1231)
+/*Sample Output:
+3
+child1 PID 6595 terminating
+child2 PID 6594 terminating
+child3 PID 6596 terminating
 */
